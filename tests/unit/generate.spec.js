810f1de9699b7d793b950cf7602015035f0007b4
LehruAngela <angelalehru@gmail.com>
2019-07-31 18:03:50 +0300

    Add tests

---
+++ tests/unit/generate.spec.js
@@ -0,0 +1,37 @@
+import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
+import Generate from '../../src/components/Generate.vue';
+
+describe('Generate.vue', () => {
+  const wrapper = shallowMount(Generate);
+
+  it('sets the correct default data', () => {
+    expect(typeof Generate.data).toBe('function');
+    const defaultData = Generate.data();
+    expect(defaultData.numbers).toEqual([]);
+    expect(defaultData.items).toEqual(['Ascending', 'Descending']);
+  });
+
+  it('correctly sets the number of generated phone numbers when mounted', () => {
+    const vm = new Vue(Generate).$mount();
+    expect(vm.numbersLeft).toEqual(10000);
+  });
+
+  it('correctly generates the number of phone numbers given', () => {
+    wrapper.vm.range = 10;
+    wrapper.vm.generateNumbers();
+    expect(wrapper.vm.numbers.length).toEqual(10);
+  });
+
+  it('correctly sorts a list of phone numbers generated in ascending order', () => {
+    wrapper.vm.numbers = ['0999999999', '0123456789', '0987654321', '0000000000'];
+    wrapper.vm.sortNumbers('Ascending');
+    expect(wrapper.vm.numbers).toEqual(['0000000000', '0123456789', '0987654321', '0999999999']);
+  });
+
+  it('correctly sorts a list of phone numbers generated in descending order', () => {
+    wrapper.vm.numbers = ['0999999999', '0123456789', '0987654321', '0000000000'];
+    wrapper.vm.sortNumbers('Descending');
+    expect(wrapper.vm.numbers).toEqual(['0999999999', '0987654321', '0123456789', '0000000000']);
+  });
+});
